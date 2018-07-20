1fa816bf730b3d2f25d024437c8f6915e875b6d9
ejhigson <18406298+ejhigson@users.noreply.github.com>
2018-07-20 10:43:35 +0100

    adding module setup.py and .gitignore

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+"""diagnostics setup."""
+import setuptools
+
+
+setuptools.setup(name='diagnostics',
+                 version='0.0.0',
+                 author='Edward Higson',
+                 install_requires=['bsr'],
+                 packages=['diagnostics'])
