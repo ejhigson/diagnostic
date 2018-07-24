e99a864e04695d2d25abf7fb86b8e3afba7a5cc0
ejhigson <18406298+ejhigson@users.noreply.github.com>
2018-07-24 10:24:59 +0100

    tidying

--- diagnostics/results_utils.py
+++ diagnostics/results_utils.py
@@ -8,14 +8,50 @@ import nestcheck.estimators as e
 import dyPolyChord.python_likelihoods as likelihoods
 
 
-R_LABEL = r'$|\theta|$'
-LOGZ_LABEL = e.get_latex_name(e.logz)
+# Settings
+# --------
 
+
+def get_default_lims(like_name, ndim_max=20):
+    """Get some default param limits for the likelihoods used in the paper."""
+    dim_labels = param_list_given_dim(ndim_max)
+    gaussian_lims = [-4, 4]  # define as used in multiple likelihoods
+    lims = {}
+    if like_name in ['LogGamma mix', 'LogGammaMix']:
+        lims[dim_labels[0]] = [-20, 20]
+        lims[dim_labels[1]] = [-20, 20]
+        lims[r'$|\theta|$'] = [10, 20]
+        if len(dim_labels) > 2:
+            assert len(dim_labels) % 2 == 0, len(dim_labels)
+            boundry = (len(dim_labels) // 2) + 1
+            for i in range(2, len(dim_labels)):
+                if i <= boundry:
+                    # LogGamma
+                    lims[dim_labels[i]] = [-7, 3]
+                else:
+                    # Gaussian
+                    lims[dim_labels[i]] = gaussian_lims
+    elif like_name == 'Gaussian':
+        for lab in dim_labels:
+            lims[lab] = gaussian_lims
+        lims[r'$|\theta|$'] = [0, 6]
+    else:
+        raise AssertionError(
+            'likename={} does not have default limits'.format(like_name))
+    return lims
+
+
+def default_logx_min(likelihood_name, ndim):
+    """Default parameter for logx_min in diagrams."""
+    return -ndim * 4
+
+def get_default_nd_nl_nr():
+    return (10, 200, 10)
 
 def get_nd_nl_nr_list(**kwargs):
     """Get list of (dim, nlive, nrepeats) tuples."""
-    defaults = kwargs.pop('defaults', (10, 200, 10))
-    nd_list = kwargs.pop('nd_list', [2, 5, 10])
+    defaults = get_default_nd_nl_nr()
+    nd_list = kwargs.pop('nd_list', [2, 4, 10])
     nl_list = kwargs.pop('nl_list', [10, 20, 50, 200, 500, 1000])
     nr_list = kwargs.pop('nr_list', [1, 2, 5, 10, 20, 50, 100, 200, 500, 1000])
     nd_nl_nr_list = []
@@ -28,9 +64,17 @@ def get_nd_nl_nr_list(**kwargs):
     return list(unique_everseen(nd_nl_nr_list))
 
 
+# Estimators
+# ----------
+
+
 def component_value(theta, ind=0):
     """Get parameter value from theta array given component index."""
-    return theta[:, ind]
+    try:
+        return theta[:, ind]
+    except IndexError:
+        assert theta.ndim == 2
+        return np.full(theta[0], np.nan)
 
 
 def radius(theta):
@@ -39,19 +83,18 @@ def radius(theta):
     return np.sqrt(np.sum(theta ** 2, axis=1))
 
 
-def default_logx_min(likelihood_name, ndim):
-    """Default parameter for logx_min in diagrams."""
-    return -ndim * 4
-
-
 def get_ftheta_list(labels_in, ndim_max=20):
     """Get a list of ftheta functions.
 
     Each ftheta maps a 2d theta array to the function value for each row."""
-    ftheta_dict = {R_LABEL: radius}
+    ftheta_dict = {r'$|\theta|$': radius}
     for i, lab in enumerate(param_list_given_dim(ndim_max)):
         ftheta_dict[lab] = functools.partial(component_value, ind=i)
     return [ftheta_dict[lab] for lab in labels_in]
+
+
+# Names
+# -----
 
 
 def param_latex_name(i):
@@ -68,33 +111,8 @@ def param_list_given_dim(ndim):
     return [param_latex_name(i) for i in range(1, ndim + 1)]
 
 
-def get_default_lims(like_name, ndim_max=20):
-    """Get some default param limits for the likelihoods used in the paper."""
-    dim_labels = param_list_given_dim(ndim_max)
-    gaussian_lims = [-4, 4]  # define as used in multiple likelihoods
-    lims = {}
-    if like_name in ['LogGamma mix', 'LogGammaMix']:
-        lims[dim_labels[0]] = [-20, 20]
-        lims[dim_labels[1]] = [-20, 20]
-        lims[R_LABEL] = [10, 20]
-        if len(dim_labels) > 2:
-            assert len(dim_labels) % 2 == 0, len(dim_labels)
-            boundry = (len(dim_labels) // 2) + 1
-            for i in range(2, len(dim_labels)):
-                if i <= boundry:
-                    # LogGamma
-                    lims[dim_labels[i]] = [-7, 3]
-                else:
-                    # Gaussian
-                    lims[dim_labels[i]] = gaussian_lims
-    elif like_name == 'Gaussian':
-        for lab in dim_labels:
-            lims[lab] = gaussian_lims
-        lims[R_LABEL] = [0, 6]
-    else:
-        raise AssertionError(
-            'likename={} does not have default limits'.format(like_name))
-    return lims
+# Analytic values
+# ---------------
 
 
 def get_true_values_dict(ndim=10, prior_scale=30,
@@ -104,12 +122,18 @@ def get_true_values_dict(ndim=10, prior_scale=30,
     for like in likelihood_list:
         tv_dict[like] = {}
         if like == 'Gaussian':
-            tv_dict[like][LOGZ_LABEL] = ndim * -np.log(2 * prior_scale)
+            tv_dict[like][e.get_latex_name(e.logz)] = (
+                ndim * -np.log(2 * prior_scale))
+            tv_dict[like][e.get_latex_name(e.evidence)] = (
+                (2 * prior_scale) ** (-ndim))
             for i in range(ndim):
                 tv_dict[like][e.get_latex_name(
                     e.param_mean, param_ind=i)] = 0
         elif like == 'LogGamma mix':
-            tv_dict[like][LOGZ_LABEL] = ndim * -np.log(2 * prior_scale)
+            tv_dict[like][e.get_latex_name(e.logz)] = (
+                ndim * -np.log(2 * prior_scale))
+            tv_dict[like][e.get_latex_name(e.evidence)] = (
+                (2 * prior_scale) ** (-ndim))
             loggamma_mean = scipy.integrate.quad(
                 lambda x: x * np.exp(likelihoods.log_loggamma_pdf_1d(x)),
                 -prior_scale, prior_scale)[0]
