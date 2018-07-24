cc96fc74178c70113dc63502bf261e1a7e68884b
ejhigson <18406298+ejhigson@users.noreply.github.com>
2018-07-24 16:15:01 +0100

    adding slurm stuff

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ import setuptools
 setuptools.setup(name='diagnostics',
                  version='0.0.0',
                  author='Edward Higson',
-                 install_requires=['bsr'],
+                 install_requires=['bsr', 'more_itertools'],
                  packages=['diagnostics'])
