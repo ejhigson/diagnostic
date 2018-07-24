1b3b3e1df1c8f48346440a9b60f90e4cacb9d8cb
ejhigson <18406298+ejhigson@users.noreply.github.com>
2018-07-24 10:26:44 +0100

    separating settings into differnt file

--- diagnostics/results_utils.py
+++ diagnostics/results_utils.py
@@ -1,67 +1,10 @@
 #!/usr/bin/env python
 """Functions for making the plots in the paper."""
 import functools
-from more_itertools import unique_everseen
 import numpy as np
 import scipy.integrate
 import nestcheck.estimators as e
 import dyPolyChord.python_likelihoods as likelihoods
-
-
-# Settings
-# --------
-
-
-def get_default_lims(like_name, ndim_max=20):
-    """Get some default param limits for the likelihoods used in the paper."""
-    dim_labels = param_list_given_dim(ndim_max)
-    gaussian_lims = [-4, 4]  # define as used in multiple likelihoods
-    lims = {}
-    if like_name in ['LogGamma mix', 'LogGammaMix']:
-        lims[dim_labels[0]] = [-20, 20]
-        lims[dim_labels[1]] = [-20, 20]
-        lims[r'$|\theta|$'] = [10, 20]
-        if len(dim_labels) > 2:
-            assert len(dim_labels) % 2 == 0, len(dim_labels)
-            boundry = (len(dim_labels) // 2) + 1
-            for i in range(2, len(dim_labels)):
-                if i <= boundry:
-                    # LogGamma
-                    lims[dim_labels[i]] = [-7, 3]
-                else:
-                    # Gaussian
-                    lims[dim_labels[i]] = gaussian_lims
-    elif like_name == 'Gaussian':
-        for lab in dim_labels:
-            lims[lab] = gaussian_lims
-        lims[r'$|\theta|$'] = [0, 6]
-    else:
-        raise AssertionError(
-            'likename={} does not have default limits'.format(like_name))
-    return lims
-
-
-def default_logx_min(likelihood_name, ndim):
-    """Default parameter for logx_min in diagrams."""
-    return -ndim * 4
-
-def get_default_nd_nl_nr():
-    return (10, 200, 10)
-
-def get_nd_nl_nr_list(**kwargs):
-    """Get list of (dim, nlive, nrepeats) tuples."""
-    defaults = get_default_nd_nl_nr()
-    nd_list = kwargs.pop('nd_list', [2, 4, 10])
-    nl_list = kwargs.pop('nl_list', [10, 20, 50, 200, 500, 1000])
-    nr_list = kwargs.pop('nr_list', [1, 2, 5, 10, 20, 50, 100, 200, 500, 1000])
-    nd_nl_nr_list = []
-    for nd in nd_list:
-        nd_nl_nr_list.append((nd, defaults[1], defaults[2]))
-    for nl in nl_list:
-        nd_nl_nr_list.append((defaults[0], nl, defaults[2]))
-    for nr in nr_list:
-        nd_nl_nr_list.append((defaults[0], defaults[1], nr))
-    return list(unique_everseen(nd_nl_nr_list))
 
 
 # Estimators
